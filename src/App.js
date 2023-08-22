f9d6b515ec9cbbe3fd117f460ad64f2369709e35
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-22 15:48:13 -0300

    feat: rename and add new styles on app.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  color: #ffffff;
+  background-color: #023047;
+  display: flex;
+`;
+
+export const CardApresentation = styled.div`
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CardInfo = styled.div`
+  width: 70%;
+`;
