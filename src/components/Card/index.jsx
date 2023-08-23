3d396ad6438fb59013bf3a690a59854deb9e22d5
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-23 17:34:22 -0300

    feat: update card component to media query compatibility

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,16 @@
+import DeveloperPhoto from '../../assets/developerPhoto.jpg';
 import {
   CardContainer,
   CardHeader,
   HeaderTitle,
   HeaderSubTitle,
+  ImageContent,
+  InformationText,
+  IconButtonStyled,
+  ButtonsContainer,
+  GithubIcon,
+  LinkedinIcon,
+  VSIcon,
 } from './styles';
 
 export const Card = () => {
@@ -13,6 +21,53 @@ export const Card = () => {
 
         <HeaderSubTitle>Web Developer</HeaderSubTitle>
       </CardHeader>
+
+      <ImageContent alt="developer image" src={DeveloperPhoto} />
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          textAlign: 'center',
+          gap: '1rem',
+        }}
+      >
+        <InformationText>otaviodev08@gmail.com</InformationText>
+        <InformationText>Belém / Brasil </InformationText>
+      </div>
+
+      <ButtonsContainer>
+        <a
+          href="https://github.com/otavioaraujo08"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconButtonStyled>
+            <GithubIcon />
+          </IconButtonStyled>
+        </a>
+
+        <a
+          href="https://br.linkedin.com/in/ot%C3%A1vio-araujo-77474b1ab?trk=people-guest_people_search-card"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconButtonStyled>
+            <LinkedinIcon />
+          </IconButtonStyled>
+        </a>
+
+        <a
+          href="https://github.com/otavioaraujo08/portfolio-"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconButtonStyled>
+            <VSIcon />
+          </IconButtonStyled>
+        </a>
+      </ButtonsContainer>
     </CardContainer>
   );
 };
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,13 +1,30 @@
 import styled from 'styled-components';
+import { DiGithubBadge } from 'react-icons/di';
+import { SiLinkedin } from 'react-icons/si';
+import { VscRepo } from 'react-icons/vsc';
 
 export const CardContainer = styled.div`
   width: 90%;
-  height: 93%;
-  background-color: #002844;
+  height: 45rem;
+  background-color: #161d34;
   border-radius: 1rem;
   border: 1px solid #3d3d4e;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+
+  @media (max-width: 1240px) {
+    height: 40rem;
+  }
+
+  @media (max-width: 890px) {
+    height: 33rem;
+  }
+
+  @media (max-width: 770px) {
+    height: 29rem;
+  }
 `;
 
 export const CardHeader = styled.div`
@@ -26,8 +43,21 @@ export const HeaderTitle = styled.h1`
   letter-spacing: 0.1rem;
   font-family: 'Hanken Grotesk', sans-serif;
   font-family:
-  color: #ffffff;
+  color: #eeeeee;
   margin-left: 1.5rem;
+
+  @media (max-width: 1200px) {
+    font-size: 1.2rem;
+    margin-left: 1rem;
+  }
+
+  @media (max-width: 900px) {
+    font-size: 1rem;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 0.7rem;
+  }
 `;
 
 export const HeaderSubTitle = styled.h2`
@@ -37,12 +67,162 @@ export const HeaderSubTitle = styled.h2`
   letter-spacing: 0.1rem;
   font-family: 'Hanken Grotesk', sans-serif;
   font-family:
-  color: #3d3d4e;
+  color: #eeeeee;
   margin-right: 1.5rem;
+
+  @media (max-width: 1200px) {
+    font-size: 0.8rem;
+  }
+
+  @media (max-width: 900px) {
+    font-size: 0.6rem;
+  }
+
+  @media (max-width: 760px) {
+    font-size: 0.8rem;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 0.6rem;
+  }
 `;
 
 export const ImageContent = styled.img`
-  width: 100%;
-  height: 100%;
+  width: fit-content;
+  height: 20rem;
   border-radius: 1rem;
+
+  @media (max-width: 1250px) {
+    height: 15rem;
+  }
+
+  @media (max-width: 900px) {
+    height: 10rem;
+  }
+
+  @media (max-width: 768px) {
+    height: 8rem;
+  }
+`;
+
+export const InformationText = styled.p`
+  font-size: 1.2rem;
+  font-weight: 400;
+  letter-spacing: 0.1rem;
+  font-family: 'Hanken Grotesk', sans-serif;
+  font-family:
+  color: #eeeeee;
+
+  @media (max-width: 1200px) {
+    font-size: 1rem;
+  }
+
+  @media (max-width: 900px) {
+    font-size: 0.8rem;
+  }
+
+  @media (max-width: 760px) {
+    font-size: 1rem;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 0.8rem;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const IconButtonStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3rem;
+  height: 3rem;
+  margin-right: 1rem;
+  border-radius: 50%;
+  background: linear-gradient(90deg, #3d3d4e 0%, #3d3d4e 100%);
+  border: 3px solid #eeeeee;
+  cursor: pointer;
+
+  &:hover {
+    border: 3px solid #c32e58;
+  }
+
+  @media (max-width: 1200px) {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  @media (max-width: 900px) {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
+  @media (max-width: 760px) {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  @media (max-width: 400px) {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`;
+
+export const GithubIcon = styled(DiGithubBadge)`
+  width: 2.2rem;
+  height: 2.2rem;
+  color: #eeeeee;
+
+  &:hover {
+    color: #c32e58;
+  }
+
+  @media (max-width: 1200px) {
+    width: 1.6rem;
+    height: 1.6rem;
+  }
+
+  @media (max-width: 900px) {
+    width: 1.3rem;
+    height: 1.3rem;
+  }
+`;
+
+export const LinkedinIcon = styled(SiLinkedin)`
+  width: 1.5rem;
+  height: 1.5rem;
+  color: #eeeeee;
+
+  &:hover {
+    color: #c32e58;
+  }
+
+  @media (max-width: 1200px) {
+    width: 1.1rem;
+    height: 1.1rem;
+  }
+
+  @media (max-width: 900px) {
+    width: 0.9rem;
+    height: 0.9rem;
+  }
+`;
+
+export const VSIcon = styled(VscRepo)`
+  width: 2rem;
+  height: 2rem;
+  color: #eeeeee;
+
+  &:hover {
+    color: #c32e58;
+  }
+
+  @media (max-width: 1200px) {
+    width: 1.4rem;
+    height: 1.4rem;
+  }
 `;
