e8db035d9bef92fa9ebbced115371dfd408f8b03
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-24 16:32:44 -0300

    feat: update app page and styles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,38 @@ export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   height: 100%;
-  color: #ffffff;
-  background-color: #023047;
+  color: #eeeeee;
+  background-color: #161d34;
   display: flex;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const CardApresentation = styled.div`
   width: 30%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  padding-top: 1.5rem;
+  padding-left: 1.5rem;
+
+  & > div {
+    position: sticky;
+    top: 15px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding-left: 0;
+  }
 `;
 
 export const CardInfo = styled.div`
   width: 70%;
+  display: flex;
+  flex-direction: column;
 `;
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { CardApresentation, CardInfo, Container } from './App';
+import { Apresentation } from './components/Apresentation';
 import { Card } from './components/Card';
 
 function App() {
   return (
     <Container>
       <CardApresentation className="card">
-        <Card />
+        <div>
+          <Card />
+        </div>
       </CardApresentation>
 
-      <CardInfo className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <CardInfo>
+        <Apresentation />
       </CardInfo>
     </Container>
   );
