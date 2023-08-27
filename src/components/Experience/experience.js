1389591bcec8f2fbea30c478564872d316e45f64
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-26 23:00:23 -0300

    feat: create experience object list

---
+++ src/components/Experience/experience.js
@@ -0,0 +1,30 @@
+export const experienceList = [
+  {
+    id: 1,
+    title: 'Desenvolvedor Front-end',
+    company: 'Frexco Tecnologia',
+    companyUrl: 'https://www.frexco.com.br',
+    date: '2022 - 2023',
+  },
+  {
+    id: 2,
+    title: 'Desenvolvedor Full-stack',
+    company: 'Cs - Consoft',
+    companyUrl: 'http://cs-consoft.com.br',
+    date: '2021 - 2022',
+  },
+  {
+    id: 3,
+    title: 'Desenvolvedor Full-stack',
+    company: 'SEGUP - Secretaria de Segurança Pública do Pará',
+    companyUrl: 'https://www.segup.pa.gov.br',
+    date: '2021 - 2021',
+  },
+  {
+    id: 4,
+    title: 'Bacharelado em Ciência da Computação',
+    company: 'Cesupa - Centro Universitário do Estado do Pará',
+    companyUrl: 'https://www.cesupa.br',
+    date: '2019 - 2022',
+  },
+];
