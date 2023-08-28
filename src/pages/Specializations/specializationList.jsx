5f866f4cc9c77805b026960fcfbc6ec983f15ccd
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-28 18:14:58 -0300

    feat: create specialization list

---
+++ src/pages/Specializations/specializationList.jsx
@@ -0,0 +1,26 @@
+import { AiOutlineAntDesign } from 'react-icons/ai';
+import { BsCodeSlash, BsListTask } from 'react-icons/bs';
+
+export const specializationList = [
+  {
+    id: 1,
+    name: 'Design de Interfaces',
+    icon: <AiOutlineAntDesign size={20} color="#c32e58" />,
+    description:
+      'Projeto interfaces para aplicativos e sites, com foco na experiência do usuário.',
+  },
+  {
+    id: 2,
+    name: 'Desenvolvimento de Aplicativos e Sites',
+    icon: <BsCodeSlash size={20} color="#c32e58" />,
+    description:
+      'Desenvolvo aplicativos e sites utilizando as tecnologias mais modernas e rápidas do mercado.',
+  },
+  {
+    id: 3,
+    name: 'Organização de Informações',
+    icon: <BsListTask size={20} color="#c32e58" />,
+    description:
+      'Organizo informações para facilitar o acesso e a compreensão com ferramentas modernas.',
+  },
+];
