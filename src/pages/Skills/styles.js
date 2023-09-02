25d9614c25665cab6e916a57a038be970d061e02
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-09-01 22:10:04 -0300

    fix: height size on Skills page

--- src/pages/Skills/styles.js
+++ src/pages/Skills/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 100%;
-  min-height: 100vh;
+  min-height: 120vh;
   width: 100%;
   display: flex;
   justify-content: center;
