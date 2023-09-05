035de7e6ac816fa3ce7a7e03d843509bee9ef412
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-09-05 18:37:21 -0300

    feat: add translated specializations

--- src/pages/Specializations/specializationList.jsx
+++ src/pages/Specializations/specializationList.jsx
@@ -5,22 +5,31 @@ export const specializationList = [
   {
     id: 1,
     name: 'Design de Interfaces',
+    nameTranslated: 'Interface Designer',
     icon: <AiOutlineAntDesign size={20} color="#c32e58" />,
     description:
       'Projeto interfaces para aplicativos e sites, com foco na experiência do usuário.',
+    descriptionTranslated:
+      'I design interfaces for applications and websites, focusing on user experience.',
   },
   {
     id: 2,
     name: 'Desenvolvimento de Aplicativos e Sites',
+    nameTranslated: 'Application and Website Developer',
     icon: <BsCodeSlash size={20} color="#c32e58" />,
     description:
       'Desenvolvo aplicativos e sites utilizando as tecnologias mais modernas e rápidas do mercado.',
+    descriptionTranslated:
+      'I develop applications and websites using the most modern and fastest technologies on the market.',
   },
   {
     id: 3,
     name: 'Organização de Informações',
+    nameTranslated: 'Information Organizer',
     icon: <BsListTask size={20} color="#c32e58" />,
     description:
       'Organizo informações para facilitar o acesso e a compreensão com ferramentas modernas.',
+    descriptionTranslated:
+      'I organize information for easy access and understanding with modern tools.',
   },
 ];
