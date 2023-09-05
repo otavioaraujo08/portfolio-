f3f6c87ed50a0c965305a112c050386123fbd379
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-09-05 17:54:25 -0300

    feat: add translation for the text on apresentation page

--- src/pages/Apresentation/index.jsx
+++ src/pages/Apresentation/index.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Insights } from '../../components/Insights';
 import { Module } from '../../components/Module';
 import { SubtitlePage } from '../../components/SubtitlePage';
@@ -5,24 +6,57 @@ import { TitlePage } from '../../components/TitlePage';
 import { ApresentationBox, Container, InsigthsBox } from './styles';
 
 export const Apresentation = () => {
+  const booleanLanguageValue = useSelector((state) => state.language);
+
   return (
     <Container>
-      <Module title="Introdução" icon="home" />
+      <Module
+        title={booleanLanguageValue ? 'Introduction' : 'Introdução'}
+        icon="home"
+      />
 
       <ApresentationBox>
         <TitlePage>
-          Olá, me chamo <strong>Otávio Araujo</strong>, e sou{' '}
-          <span>desenvolvedor de software.</span>
+          {booleanLanguageValue ? (
+            <>
+              Hello, my name is <strong>Otávio Araujo</strong>, and I'm a{' '}
+              <strong>software developer.</strong>
+            </>
+          ) : (
+            <>
+              Olá, me chamo <strong>Otávio Araujo</strong>, e sou{' '}
+              <span>desenvolvedor de software.</span>
+            </>
+          )}
         </TitlePage>
         <SubtitlePage>
-          Eu planejo, projeto e desenvolvo aplicações web e mobile.
+          {booleanLanguageValue
+            ? 'I plan, design and develop web and mobile applications.'
+            : 'Eu planejo, projeto e desenvolvo aplicações web e mobile.'}
         </SubtitlePage>
       </ApresentationBox>
 
       <InsigthsBox>
-        <Insights number="2+" text="Anos de experiência" />
-        <Insights number="800+" text="Commits no Github" />
-        <Insights number="2000+" text="Contribuições no Github" />
+        <Insights
+          number="2+"
+          text={
+            booleanLanguageValue ? 'Years of experience' : 'Anos de experiência'
+          }
+        />
+        <Insights
+          number="800+"
+          text={
+            booleanLanguageValue ? 'Commits on Github' : 'Commits no Github'
+          }
+        />
+        <Insights
+          number="2000+"
+          text={
+            booleanLanguageValue
+              ? 'Contributions on Github'
+              : 'Contribuições no Github'
+          }
+        />
       </InsigthsBox>
     </Container>
   );
