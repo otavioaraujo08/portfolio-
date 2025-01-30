f030b6731744612fd02ea344b3fdd204d19bbf14
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-30 15:13:05 -0300

    feat: add new specializations with translations and icons

--- src/pages/Specializations/specializationList.jsx
+++ src/pages/Specializations/specializationList.jsx
@@ -32,4 +32,44 @@ export const specializationList = [
     descriptionTranslated:
       'I organize information for easy access and understanding with modern tools.',
   },
+  {
+    id: 4,
+    name: 'Desenvolvimento de Testes',
+    nameTranslated: 'Test Development',
+    icon: <BsCodeSlash size={20} color="#c32e58" />,
+    description:
+      'Desenvolvo testes para aplicativos e sites, garantindo a qualidade do produto final.',
+    descriptionTranslated:
+      'I develop tests for applications and websites, ensuring the quality of the final product.',
+  },
+  {
+    id: 5,
+    name: 'Gerenciamento de Projetos',
+    nameTranslated: 'Project Management',
+    icon: <BsListTask size={20} color="#c32e58" />,
+    description:
+      'Gerencio projetos de desenvolvimento de aplicativos e sites, garantindo a entrega no prazo.',
+    descriptionTranslated:
+      'I manage projects for the development of applications and websites, ensuring delivery on time.',
+  },
+  {
+    id: 6,
+    name: 'Gerenciamento de Times Multidisciplinares',
+    nameTranslated: 'Multidisciplinary Team Management',
+    icon: <BsListTask size={20} color="#c32e58" />,
+    description:
+      'Gerencio times de desenvolvimento de aplicativos e sites, garantindo a comunicação entre as equipes.',
+    descriptionTranslated:
+      'I manage development teams for applications and websites, ensuring communication between teams.',
+  },
+  {
+    id: 7,
+    name: 'Automação de CI/CD',
+    nameTranslated: 'CI/CD Automation',
+    icon: <BsCodeSlash size={20} color="#c32e58" />,
+    description:
+      'Automatizo o processo de integração e entrega contínua de aplicativos e sites para garantir a qualidade do produto final.',
+    descriptionTranslated:
+      'I automate the process of integrating and continuously delivering applications and websites to ensure the quality of the final product.',
+  },
 ];
