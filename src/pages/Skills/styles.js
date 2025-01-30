f3d000aae4ba6a23f000bf251ae8183bc221b3e9
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-30 16:03:26 -0300

    feat: reduce minimum height of Container for improved responsiveness

--- src/pages/Skills/styles.js
+++ src/pages/Skills/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 100%;
-  min-height: 120vh;
+  min-height: 60vh;
   width: 100%;
   display: flex;
   justify-content: center;
