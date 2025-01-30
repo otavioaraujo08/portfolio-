36af7b6efb300c7c50908768bc2065e7a1e21d1f
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-30 16:03:49 -0300

    feat: add activities descriptions in Portuguese and English for experience entries

--- src/pages/Experience/experience.js
+++ src/pages/Experience/experience.js
@@ -7,6 +7,20 @@ export const experienceList = [
     companyTranslated: 'Public Defender of the State of Pará',
     companyUrl: 'https://defensoria.pa.def.br/',
     date: '2024 - 2025',
+    atividades: [
+      "Desenvolvimento de interfaces utilizando React Js, Next Js, TailwindCSS, Shadcn e Typescript",
+      "Desenvolvimento de testes unitários e ponta-a-ponta, utilizando Jest e React Testing Library",
+      "Gerenciamento de times interdisciplinares utilizando metodologias ágeis",
+      "Versionamento de código utilizando Git e plataforma Gitlab",
+      "Autogerenciamento de demandas e sprints utilizando plataforma como OpenProject"
+    ],
+    activitys: [
+      "Development of interfaces using React Js, Next Js, TailwindCSS, Shadcn and Typescript",
+      "Development of unit and end-to-end tests, using Jest and React Testing Library",
+      "Management of interdisciplinary teams using agile methodologies",
+      "Code versioning using Git and Gitlab platform",
+      "Self-management of demands and sprints using platform like OpenProject"
+    ]
   },
   {
     id: 2,
@@ -16,6 +30,22 @@ export const experienceList = [
     companyTranslated: 'Frexco Technology',
     companyUrl: 'https://www.frexco.com.br',
     date: '2022 - 2023',
+    atividades: [
+      "Boas práticas de código visando últimas atualizações do ECMAScript.",
+      "Desenvolvimento de testes ponta-ponta/unitários utilizando Jest e Selenium",
+      "Gestão de tempo em metodologias ágeis como SCRUM.",
+      "Otimização de desempenho utilizando Django e suas ferramentas",
+      "Desenvolvendo aplicações com ReactJs, NextJs e Material UI",
+      "Implementando animações web com React Spring"
+    ],
+    activitys: [
+      "Good code practices aiming at the latest updates of ECMAScript.",
+      "Development of end-to-end/unit tests using Jest and Selenium",
+      "Time management in agile methodologies like SCRUM.",
+      "Performance optimization using Django and its tools",
+      "Developing applications with ReactJs, NextJs and Material UI",
+      "Implementing web animations with React Spring"
+    ]
   },
   {
     id: 3,
@@ -25,6 +55,20 @@ export const experienceList = [
     companyTranslated: 'Cs - Consoft',
     companyUrl: 'http://cs-consoft.com.br',
     date: '2021 - 2022',
+    atividades: [
+      "Desenvolvimento de aplicações utilizando framework Django e Python.",
+      "Documentação de aplicações utilizando Swagger API",
+      "Criação de interfaces web utilizando Vue 3 e Quasar.",
+      "Experiência trabalhando com times interdisciplinares",
+      "Vivência utilizando gerenciadores de tarefas ágeis (Trello, Jira)"
+    ],
+    activitys: [
+      "Application development using Django framework and Python.",
+      "Application documentation using Swagger API",
+      "Creating web interfaces using Vue 3 and Quasar.",
+      "Experience working with interdisciplinary teams",
+      "Experience using agile task managers (Trello, Jira)"
+    ]
   },
   {
     id: 4,
@@ -34,6 +78,18 @@ export const experienceList = [
     companyTranslated: 'SEGUP - Secretary of Public Security of Pará',
     companyUrl: 'https://www.segup.pa.gov.br',
     date: '2021 - 2021',
+    atividades: [
+      "Desenvolvimento de aplicações utilizando framework Django",
+      "Integração com banco de dados utilizando PostgreSQL e Django",
+      "Aprimoramento e otimizações no sistema utilizando JavaScript",
+      "Criação de interfaces utilizando HTML, CSS e Vue JS"
+    ],
+    activitys: [
+      "Application development using Django framework",
+      "Database integration using PostgreSQL and Django",
+      "Improvements and optimizations in the system using JavaScript",
+      "Creating interfaces using HTML, CSS and Vue JS"
+    ]
   },
   {
     id: 5,
@@ -43,5 +99,7 @@ export const experienceList = [
     companyTranslated: 'Cesupa - University Center of the State of Pará',
     companyUrl: 'https://www.cesupa.br',
     date: '2019 - 2022',
+    atividades: [],
+    activitys: []
   },
 ];
