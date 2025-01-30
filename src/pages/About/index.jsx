c7982616021a4ffa633791aa6810324c5e034e90
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-30 15:13:27 -0300

    feat: update About page content and improve language consistency

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Module } from '../../components/Module';
 import { SubtitlePage } from '../../components/SubtitlePage';
 import { TitlePage } from '../../components/TitlePage';
-import { Container, ApresentationBox } from './styles';
+import { ApresentationBox, Container } from './styles';
 
 export const About = () => {
   const booleanLanguageValue = useSelector((state) => state.language);
@@ -12,55 +12,27 @@ export const About = () => {
       <Module icon="user" title={booleanLanguageValue ? 'About' : 'Sobre'} />
 
       <ApresentationBox>
-        <TitlePage
-          title={booleanLanguageValue ? 'My way...' : 'Meu trajeto...'}
-        />
+        <TitlePage title={booleanLanguageValue ? 'Resume' : 'Resumo'} />
 
         <SubtitlePage>
           {booleanLanguageValue ? (
             <>
-              Fullstack Javascript Developer with 2+ years of experience. I
-              participated in Web projects using current technologies such as{' '}
+              Fullstack Web Developer with 4+ years experience. Web using
+              current technologies such as{' '}
             </>
           ) : (
             <>
-              Desenvolvedor Javascript Fullstack com 2+ anos de experiência.
-              Participei de projetos Web utilizando tecnologias atuais como{' '}
+              Desenvolvedor Web Fullstack com 4+ anos de experiência. Participei
+              de projetos Web utilizando tecnologias atuais como{' '}
             </>
           )}
           <strong>
-            ReactJs, Redux, Vue, Nuxt, NodeJs, NextJs, Express, TypeScript,
+            ReactJs, NextJs, Vue, Nuxt, NodeJs, NestJs, Express, TypeScript,
             Jest, MongoDB, MySQL, HTML, CSS, TailwindCSS, Github
           </strong>{' '}
-          {booleanLanguageValue ? 'among others.' : `entre outras.`}
-        </SubtitlePage>
-
-        <SubtitlePage>
-          <strong>
-            {booleanLanguageValue
-              ? 'I am Bachelor of Computer Science, '
-              : `Sou Bacharel em Ciência da computação, `}
-          </strong>
           {booleanLanguageValue
-            ? 'currently working on personal projects in order to improve technical skills. '
-            : `atualmente atuando em projetos pessoais com o intuito de aprimorar
-              habilidades técnicas. `}
-        </SubtitlePage>
-
-        <SubtitlePage>
-          {booleanLanguageValue ? (
-            <>
-              <strong>Passionate</strong> about everything involving{' '}
-              <strong>technology </strong> and <strong>art</strong>, I like to
-              share and help the community learn.
-            </>
-          ) : (
-            <>
-              <strong>Apaixonado</strong> por tudo que envolve{' '}
-              <strong>tecnologia</strong> e <strong>arte</strong>, gosto de
-              compartilhar e ajudar no aprendizado da comunidade.
-            </>
-          )}
+            ? 'among others. I am a Bachelor in Computer Science; working in the Public Defender of the State of Para.'
+            : `entre outras. Sou Bacharel em Ciência da computação; atualmente atuando na Defensoria Pública do Estado do Pará.`}
         </SubtitlePage>
       </ApresentationBox>
     </Container>
