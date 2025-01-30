c4ebf7acc340d3e0ac3667083cc913e58626184d
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-30 16:03:53 -0300

    feat: update About page styles for responsive height adjustment

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   gap: 2rem;
