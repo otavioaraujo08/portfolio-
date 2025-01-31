90620661c4144cc9831bd7e51d7fe1bb7da84dcb
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-31 15:55:33 -0300

    feat: update About page styles for improved layout and alignment

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,18 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
-  gap: 2rem;
   flex-direction: column;
-  text-align: left;
-
-  @media (max-width: 740px) {
-    gap: 2rem;
-  }
+  justify-content: center;
+  align-items: left;
+  gap: 2rem;
+  min-height: 100vh;
 `;
 
 export const ApresentationBox = styled.div`
