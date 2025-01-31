edfbcceb3ce9cfee0ba5e778bfce107cd85d8b7d
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-31 15:55:45 -0300

    feat: enhance Projects page with dynamic project listing and improved layout

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,7 +1,15 @@
+import { Button } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { Module } from '../../components/Module';
 import { TitlePage } from '../../components/TitlePage';
-import { Container } from './styles';
+import { projetctList } from './projectList';
+import {
+  Container,
+  Image,
+  ProjectDiv,
+  ProjectSubTitle,
+  ProjectTitle,
+} from './styles';
 
 export const Projects = () => {
   const booleanLanguageValue = useSelector((state) => state.language);
@@ -23,6 +31,50 @@ export const Projects = () => {
           </>
         )}
       </TitlePage>
+
+      <ProjectDiv>
+        {projetctList.map((project, index) => (
+          <ProjectDiv key={project.id}>
+            {project.image ? (
+              <Image src={project.image} alt="project image" />
+            ) : null}
+
+            <ProjectDiv>
+              <ProjectTitle>{project.title}</ProjectTitle>
+              {booleanLanguageValue ? (
+                <ProjectSubTitle>{project.description}</ProjectSubTitle>
+              ) : (
+                <ProjectSubTitle>{project.descricao}</ProjectSubTitle>
+              )}
+            </ProjectDiv>
+
+            {project.api ? (
+              <a href={project.link} target="_blank" rel="noreferrer">
+                <Button variant="outlined">
+                  Redirecionar para a documentação da API
+                </Button>
+              </a>
+            ) : (
+              <a href={project.link} target="_blank" rel="noreferrer">
+                <Button
+                  variant="outlined"
+                  style={
+                    index === projetctList.length - 1
+                      ? { marginBottom: '2rem' }
+                      : {}
+                  }
+                >
+                  Redirecionar para o site do projeto
+                </Button>
+              </a>
+            )}
+
+            {index !== projetctList.length - 1 && (
+              <hr style={{ borderColor: '#ffffff', marginBottom: '2rem' }} />
+            )}
+          </ProjectDiv>
+        ))}
+      </ProjectDiv>
     </Container>
   );
 };
