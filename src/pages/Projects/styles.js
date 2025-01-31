2e5b02a747e9dbd1aa313a6ac2b9be67be158a74
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-31 15:55:29 -0300

    feat: enhance Projects styles with new layout and responsive image handling

--- src/pages/Projects/styles.js
+++ src/pages/Projects/styles.js
@@ -1,16 +1,40 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin-top: 1rem;
-  height: 100%;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
-  gap: 2rem;
   flex-direction: column;
-  text-align: left;
+  gap: 2rem;
+`;
 
-  @media (max-width: 740px) {
-    height: 90vh;
-  }
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  max-width: 60rem;
+  max-height: 60rem;
+  border-radius: 1rem;
+  object-fit: cover;
+`;
+
+export const ProjectDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+export const ProjectTitle = styled.h2`
+  font-size: 2rem;
+  font-weight: 700;
+  letter-spacing: 0.1rem;
+  font-family: 'Roboto', sans-serif;
+  color: #fff;
+  text-align: left;
+`;
+
+export const ProjectSubTitle = styled.h2`
+  font-size: 1.2rem;
+  font-weight: 500;
+  letter-spacing: 0.1rem;
+  font-family: 'Roboto', sans-serif;
+  color: #c4c4c4;
+  text-align: left;
 `;
